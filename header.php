d019fee1c259d2220cbf167a96cf7885d8f5bc5e
leGromp12 <kochala.oleksandr@chnu.edu.ua>
2026-03-23 23:19:47 +0200

    fixed style.css

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Board Game Shop</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/boardgames_shop/style.css?v=1">
 </head>
 <body>
 
